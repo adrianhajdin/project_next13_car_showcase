f9b7611641d63ab37a6b4175244d62e3b0764957
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-05-20 16:58:23 +0530

    filter params

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,40 @@
 import CarCard from "@components/CarCard";
+import Filter from "@components/Filter";
+import { manufacturers, yearsOfProduction } from "@constants";
 
 export default function Home() {
   return (
     <main>
-      <CarCard
-        model='sedan'
-        make='toyota'
-        mpg={30}
-        transmission='a'
-        year={2022}
-        drive='front'
-      />
+      <div className='flex max-md:flex-col md:justify-between max-md:items-start w-full pt-16'>
+        <div className='flex flex-col items-start justify-start gap-y-2.5 text-black-400'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+
+        <div className='flex justify-start flex-wrap items-center gap-2 max-md:pt-10'>
+          <Filter title='Fuel Type' options={["Gas", "Electricity"]} />
+          <Filter title='Manufacturer' options={manufacturers} />
+          <Filter title='Year' options={yearsOfProduction} />
+          {/* <button
+            type='button'
+            className='border-[1px] rounded-full p-[2px] w-[20px] h-[20px]'
+            onClick={clearFilters}
+          >
+            <img src='/assets/icons/close.svg' alt='close' />
+          </button> */}
+        </div>
+      </div>
+
+      <div className='grid 2xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-2 grid-cols-1 w-full gap-8 pt-14'>
+        <CarCard
+          model='sedan'
+          make='toyota'
+          mpg={30}
+          transmission='a'
+          year={2022}
+          drive='front'
+        />
+      </div>
     </main>
   );
 }
---
+++ components/Filter.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+interface FilterProps {
+  title: string;
+  options: string[];
+}
+
+const Filter = ({ title, options }: FilterProps) => {
+  const router = useRouter();
+
+  const [openModal, setOpenModal] = useState(false);
+  const [selected, setSelected] = useState<string | null>(null);
+
+  const handleClick = (e: React.SyntheticEvent) => {
+    const target = e.target as HTMLInputElement;
+    setSelected(target.value);
+
+    // add as params
+    updateSearchParams(target.value);
+  };
+
+  const updateSearchParams = (selectedItem: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const updatedParams = new URLSearchParams(searchParams.toString());
+
+    const lowercaseTitleName = title.toLowerCase();
+    const lowercaseSelectedItem = selectedItem.toLowerCase();
+
+    if (updatedParams.has(lowercaseTitleName)) {
+      updatedParams.set(lowercaseTitleName, lowercaseSelectedItem);
+    } else {
+      updatedParams.append(lowercaseTitleName, lowercaseSelectedItem);
+    }
+
+    const newPathname = `${
+      window.location.pathname
+    }?${updatedParams.toString()}`;
+
+    router.push(newPathname);
+  };
+
+  return (
+    <div
+      onClick={() => setOpenModal(!openModal)}
+      className='flex flex-col items-center relative text-[14px] leading-[17px] justify-center cursor-pointer'
+    >
+      <div className='flex w-max justify-center items-center rounded-lg border-black-300 border-[1px]'>
+        <button
+          type='button'
+          className='py-3 px-4 flex justify-center items-center gap-2'
+        >
+          {title}
+          <div
+            className={`${
+              !selected && "hidden"
+            } w-[10px] h-[10px] bg-secondary-orange rounded-full`}
+          />
+        </button>
+        <img
+          src='/arrow-down.svg'
+          className={`${openModal ? "pl-[18px] rotate-180" : "pr-[18px]"}`}
+          alt='down arrow'
+        />
+      </div>
+
+      {openModal && (
+        <div
+          className='flex flex-col absolute top-12 justify-start items-start w-full max-h-[200px] snap-y overflow-auto outline-0 border-[1px] border-black-300 bg-white-600 rounded-lg'
+          defaultValue='default'
+        >
+          {options?.map((option) => (
+            <button
+              key={option}
+              type='button'
+              value={option}
+              className={`${
+                selected === option && "font-bold"
+              } snap-center hover:bg-[#efefef] w-full p-2`}
+              onClick={(e) => handleClick(e)}
+            >
+              {option}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Filter;
