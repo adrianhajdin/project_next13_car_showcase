ee5c56dcd601ffba1c6c24726c64a22e3fd143e1
TidbitsJS <tidbitjs@gmail.com>
2023-05-21 10:29:39 -0700

    tailwind colors

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -18,63 +18,63 @@ const CarCard = ({
   const carRent = calculateCarRent(cityMPG, year);
 
   return (
-    <div className='group flex flex-col p-6 justify-center items-start text-black-400 bg-[rgba(59,60,152,0.02)] rounded-[24px] hover:shadow-md'>
-      <h2 className='text-[22px] leading-[26px] font-bold'>
+    <div className="group flex flex-col p-6 justify-center items-start text-black-400 bg-light-white-100 rounded-[24px] hover:shadow-md">
+      <h2 className="text-[22px] leading-[26px] font-bold">
         {make.charAt(0).toUpperCase() + make.slice(1)}{" "}
         {model.charAt(0).toUpperCase() + model.slice(1)}
       </h2>
 
-      <p className='flex mt-6 text-[32px] leading-[38px] font-extrabold'>
-        <span className='self-start text-[14px] leading-[17px] font-semibold'>
+      <p className="flex mt-6 text-[32px] leading-[38px] font-extrabold">
+        <span className="self-start text-[14px] leading-[17px] font-semibold">
           $
         </span>
         {carRent}
-        <span className='self-end text-[14px] leading-[17px] font-medium'>
+        <span className="self-end text-[14px] leading-[17px] font-medium">
           /day
         </span>
       </p>
 
-      <div className='relative w-full h-40 my-4 object-contain'>
+      <div className="relative w-full h-40 my-4 object-contain">
         <Image
           src={`https://cdn.imagin.studio/getimage?customer=${imaginApiKey}&make=${make}&modelFamily=${
             model.split(" ")[0]
           }&zoomType=fullscreen&modelYear=${year}`}
-          alt='car model'
+          alt="car model"
           fill
           priority
-          className='object-contain'
+          className="object-contain"
         />
       </div>
 
-      <div className='relative flex w-full mt-2'>
-        <div className='flex w-full justify-between text-[#747A88]'>
-          <div className='flex flex-col justify-center items-center gap-2'>
+      <div className="relative flex w-full mt-2">
+        <div className="flex w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
             <Image
-              src='/steering-wheel.svg'
+              src="/steering-wheel.svg"
               width={20}
               height={20}
-              alt='steering wheel'
+              alt="steering wheel"
             />
-            <p className='text-[14px] leading-[17px]'>
+            <p className="text-[14px] leading-[17px]">
               {transmission === "a" ? "Automatic" : "Manual"}
             </p>
           </div>
-          <div className='flex flex-col justify-center items-center gap-2'>
-            <Image src='/tire.svg' width={20} height={20} alt='seat' />
-            <p className='text-[14px] leading-[17px]'>{drive.toUpperCase()}</p>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tire.svg" width={20} height={20} alt="seat" />
+            <p className="text-[14px] leading-[17px]">{drive.toUpperCase()}</p>
           </div>
-          <div className='flex flex-col justify-center items-center gap-2'>
-            <Image src='/gas.svg' width={20} height={20} alt='seat' />
-            <p className='text-[14px] leading-[17px]'>{mpg} MPG</p>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/gas.svg" width={20} height={20} alt="seat" />
+            <p className="text-[14px] leading-[17px]">{mpg} MPG</p>
           </div>
         </div>
 
-        <div className='hidden group-hover:flex absolute bottom-0 w-full z-10'>
+        <div className="hidden group-hover:flex absolute bottom-0 w-full z-10">
           <CustomButton
-            title='Rent Now'
-            containerStyles='w-full py-[16px] rounded-lg bg-gradient-to-r from-[#5E60C1] from-[0.78%] to-[#7E80CD] to-[99.38%]'
-            textStyles='text-white text-[14px] leading-[17px] font-bold'
-            rightIcon='/right-arrow.svg'
+            title="Rent Now"
+            containerStyles="w-full py-[16px] rounded-lg bg-gradient-to-r from-[#5E60C1] from-[0.78%] to-[#7E80CD] to-[99.38%]"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
           />
         </div>
       </div>
