d3c99ea42dc87d193b6468ecaeaebfcbc4cfe907
tidbitsjs <tidbitjs@gmail.com>
2023-05-21 11:41:23 -0700

    added comments

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -15,19 +15,25 @@ const Filter = ({ title, options }: FilterItemProps) => {
   const modalRef = useRef<HTMLDivElement>(null);
 
   const handleClick = (e: React.SyntheticEvent) => {
+    // Extract the target element from the event
     const target = e.target as HTMLInputElement;
+
+    // Check if the clicked option is already selected
     if (selected === target.value) {
+      // Deselect the option by setting selected state to an empty string
       setSelected("");
 
+      // Delete the corresponding search parameter from the URL
       const newPathName = deleteSearchParams(title);
       router.push(newPathName);
 
       return;
     }
 
+    // Set the selected state to the value of the clicked option
     setSelected(target.value);
 
-    // add as params
+    // Convert the title to lowercase and add it as a search parameter in the URL
     const newPathName = updateSearchParams(
       title.toLowerCase(),
       target.value.toLowerCase()
@@ -38,7 +44,9 @@ const Filter = ({ title, options }: FilterItemProps) => {
 
   // close modal when clicked outside
   useEffect(() => {
+    // Function to handle the click outside the modal
     const handleClickOutside = (event: MouseEvent) => {
+      // Check if the modal reference exists and if the clicked element is outside the modal
       if (
         modalRef.current &&
         !modalRef.current.contains(event.target as Node)
@@ -47,7 +55,10 @@ const Filter = ({ title, options }: FilterItemProps) => {
       }
     };
 
+    // Add event listener for mousedown event to detect clicks outside the modal
     document.addEventListener("mousedown", handleClickOutside);
+
+    // Clean up the event listener when the component is unmounted
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
@@ -57,12 +68,12 @@ const Filter = ({ title, options }: FilterItemProps) => {
     <div
       ref={modalRef}
       onClick={() => setOpenModal(!openModal)}
-      className='flex flex-col items-center relative text-[14px] leading-[17px] justify-center cursor-pointer'
+      className="flex flex-col items-center relative text-[14px] leading-[17px] justify-center cursor-pointer"
     >
-      <div className='flex w-max justify-center items-center gap-3 px-4 py-2.5 rounded-lg border-black-300 border-[1px]'>
+      <div className="flex w-max justify-center items-center gap-3 px-4 py-2.5 rounded-lg border-black-300 border-[1px]">
         <button
-          type='button'
-          className='flex justify-center items-center gap-2'
+          type="button"
+          className="flex justify-center items-center gap-2"
         >
           {title}
           <div
@@ -72,23 +83,23 @@ const Filter = ({ title, options }: FilterItemProps) => {
           />
         </button>
         <Image
-          src='/arrow-down.svg'
+          src="/arrow-down.svg"
           width={10}
           height={10}
           className={`${openModal ? "rotate-180" : ""}`}
-          alt='down arrow'
+          alt="down arrow"
         />
       </div>
 
       {openModal && (
         <div
-          className='flex flex-col absolute top-12 justify-start items-start max-h-[200px] snap-y overflow-auto outline-0 border-[1px] border-black-300 bg-white-600 rounded-lg z-10 text-left'
-          defaultValue='default'
+          className="flex flex-col absolute top-12 justify-start items-start max-h-[200px] snap-y overflow-auto outline-0 border-[1px] border-black-300 bg-white-600 rounded-lg z-10 text-left"
+          defaultValue="default"
         >
           {options?.map((option) => (
             <button
               key={option}
-              type='button'
+              type="button"
               value={option}
               className={`${
                 selected === option ? "font-bold text-black" : "text-gray-600"
--- utils/index.ts
+++ utils/index.ts
@@ -16,20 +16,26 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 };
 
 export const updateSearchParams = (type: string, value: string) => {
+  // Get the current URL search params
   const searchParams = new URLSearchParams(window.location.search);
-  const updatedParams = new URLSearchParams(searchParams.toString());
 
-  updatedParams.set(type, value);
+  // Set the specified search parameter to the given value
+  searchParams.set(type, value);
 
-  const newPathname = `${window.location.pathname}?${updatedParams.toString()}`;
+  // Set the specified search parameter to the given value
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
 
   return newPathname;
 };
 
 export const deleteSearchParams = (type: string) => {
+  // Set the specified search parameter to the given value
   const newSearchParams = new URLSearchParams(window.location.search);
+
+  // Delete the specified search parameter
   newSearchParams.delete(type.toLocaleLowerCase());
 
+  // Construct the updated URL pathname with the deleted search parameter
   const newPathname = `${
     window.location.pathname
   }?${newSearchParams.toString()}`;
@@ -40,17 +46,21 @@ export const deleteSearchParams = (type: string) => {
 export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
 
+  // Set the required headers for the API request
   const headers: HeadersInit = {
     "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
+  // Set the required headers for the API request
   const response = await fetch(
     `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
     }
   );
+
+  // Parse the response as JSON
   const result = await response.json();
   return result;
 }
