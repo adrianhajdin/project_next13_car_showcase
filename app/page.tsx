8be3ed70ddf46f443ef20623977dcdd5da8ae484
tidbitsjs <tidbitjs@gmail.com>
2023-05-21 08:12:13 -0700

    error handling

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,25 @@ export default async function Home({ searchParams }: HomeProps) {
     model: searchParams.model || "",
   });
 
+  console.log(allCars, allCars.length);
+
+  if (!Array.isArray(allCars))
+    return (
+      <main className="sm:px-16 px-6 py-4 min-h-[50vh]">
+        <SearchBar />
+
+        <div className="mt-16 flex justify-center items-center flex-col gap-2">
+          <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+          <p>{allCars?.message}</p>
+        </div>
+      </main>
+    );
+
   return (
     <main className="sm:px-16 px-6 py-4">
       <SearchBar />
 
-      <div className="flex max-md:flex-col md:justify-between max-md:items-start w-full pt-16">
+      <div className="flex max-md:flex-col md:justify-between max-md:items-start w-full mt-16">
         <div className="flex flex-col items-start justify-start gap-y-2.5 text-black-400">
           <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore out cars you might like</p>
@@ -31,7 +45,7 @@ export default async function Home({ searchParams }: HomeProps) {
 
       <section className="flex flex-col w-full h-full">
         <div className="grid 2xl:grid-cols-5 xl:grid-cols-4 md:grid-cols-2 grid-cols-1 w-full gap-8 pt-14">
-          {allCars.map((car: CarProps) => (
+          {allCars?.map((car: CarProps) => (
             <CarCard
               model={car.model}
               make={car.make}
