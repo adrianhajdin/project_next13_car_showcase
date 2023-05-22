4ea94ad329102e9b59ace79c70b5c86ec7e55a5d
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-05-22 23:54:20 +0530

    search manufacturer combobox

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,104 @@
+import { Fragment, useState } from "react";
+import Image from "next/image";
+import { Combobox, Transition } from "@headlessui/react";
+
+import { manufacturers } from "@constants";
+
+const SearchManufacturer = () => {
+  const [selected, setSelected] = useState("");
+  const [query, setQuery] = useState("");
+
+  const filtereManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((manufacturer) =>
+          manufacturer
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
+  return (
+    <div className='flex-1 max-sm:w-full flex justify-start items-center'>
+      <Combobox value={selected} onChange={setSelected}>
+        <div className='relative w-full'>
+          <Combobox.Button className='absolute top-[14px]'>
+            <Image
+              src='/car-logo.svg'
+              width={25}
+              height={25}
+              className='ml-4'
+              alt='car logo'
+            />
+          </Combobox.Button>
+          <Combobox.Input
+            className='w-full h-[52px] pl-12 p-4 rounded-l-full max-sm:rounded-full bg-light-white outline-none text-white-800 cursor-pointer'
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(event) => setQuery(event.target.value)}
+            placeholder='bmw...'
+          />
+
+          <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options
+              className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
+              static
+            >
+              {filtereManufacturers.length === 0 && query !== "" ? (
+                <Combobox.Option
+                  value={query}
+                  className='cursor-default select-none py-2 pl-10 pr-4'
+                >
+                  Create "{query}"
+                </Combobox.Option>
+              ) : (
+                filtereManufacturers.map((manufacturer) => (
+                  <Combobox.Option
+                    key={manufacturer}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                        active
+                          ? "bg-primary-purple text-white"
+                          : "text-gray-900"
+                      }`
+                    }
+                    value={manufacturer}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? "font-medium" : "font-normal"
+                          }`}
+                        >
+                          {manufacturer}
+                        </span>
+
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active
+                                ? "text-white"
+                                : "text-pribg-primary-purple"
+                            }`}
+                          ></span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
+};
+
+export default SearchManufacturer;
