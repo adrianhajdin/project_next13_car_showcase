6a959afe7f325128c8125bf3532e8050e766b261
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-05-24 23:17:42 +0530

    few fixes II

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,56 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 import CustomButton from "./CustomButton";
 
-const NavBar = () => (
-  <nav className='flex justify-between items-center w-full sm:px-16 px-6 py-4'>
-    <Link href='/' className='flex justify-center items-center'>
-      <Image
-        src='/logo.svg'
-        alt='logo'
-        width={118}
-        height={18}
-        className='object-contain'
-      />
-    </Link>
+const NavBar = () => {
+  const pathName = usePathname();
 
-    <div className='flex text-[14px] leading-[17px] font-extrabold text-primary-purple'>
-      <CustomButton title='Register' btnType='button' />
-      <CustomButton
-        title='Sign in'
-        btnType='button'
-        containerStyles='max-sm:hidden px-4 py-2 border-primary-purple border-[1px] rounded-xl hover:bg-primary-purple hover:text-white'
-      />
-    </div>
-  </nav>
-);
+  return (
+    <nav className='flex justify-between items-center w-full sm:px-16 px-6 py-4'>
+      <Link href='/' className='flex justify-center items-center'>
+        <Image
+          src='/logo.svg'
+          alt='logo'
+          width={118}
+          height={18}
+          className='object-contain'
+        />
+      </Link>
+
+      <ul className='flex gap-7'>
+        <Link href='/'>
+          <li
+            className={`${
+              pathName === "/" ? "text-primary-purple" : ""
+            } text-[18px] font-medium`}
+          >
+            Rental Deals
+          </li>
+        </Link>
+        <Link href='/my-favorites'>
+          <li
+            className={`${
+              pathName === "/my-favorites" ? "text-primary-purple" : ""
+            } text-[18px] font-medium`}
+          >
+            Favorite Cars
+          </li>
+        </Link>
+      </ul>
+
+      <div className='flex text-base text-primary-purple'>
+        <CustomButton
+          title='Sign in'
+          btnType='button'
+          containerStyles='rounded-full bg-primary-purple text-white'
+        />
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
