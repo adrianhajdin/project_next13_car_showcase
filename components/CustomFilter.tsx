8f25ba9f6a619650787b14287db8d8c3b084a35d
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-05-24 18:09:06 +0530

    headlessui filter

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { Fragment, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Listbox, Transition } from "@headlessui/react";
+
+import { CustomFilterProps } from "@types";
+import { updateSearchParams } from "@utils";
+
+export default function CustomFilter({ title, options }: CustomFilterProps) {
+  const router = useRouter();
+  const [selected, setSelected] = useState(options[0]);
+
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+    router.push(newPathName);
+  };
+
+  return (
+    <div className='w-fit'>
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
+          handleUpdateParams(e);
+        }}
+      >
+        <div className='relative w-fit z-10'>
+          <Listbox.Button className='relative w-full min-w-[127px] flex justify-between items-center cursor-default rounded-lg bg-white py-2 px-3 text-left shadow-md sm:text-sm border'>
+            <span className='block truncate'>{selected.title}</span>
+            <Image
+              src='/chevron-up-down.svg'
+              width={20}
+              height={20}
+              className='ml-4 object-contain'
+              alt='chevron_up-down'
+            />
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <Listbox.Options className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
+              {options.map((option) => (
+                <Listbox.Option
+                  key={option.title}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 px-4 ${
+                      active ? "bg-primary-purple text-white" : "text-gray-900"
+                    }`
+                  }
+                  value={option}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {option.title}
+                      </span>
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
+}
--- constants/index.ts
+++ constants/index.ts
@@ -43,15 +43,31 @@ export const manufacturers = [
 ];
 
 export const yearsOfProduction = [
-  "2015",
-  "2016",
-  "2017",
-  "2018",
-  "2019",
-  "2020",
-  "2021",
-  "2022",
-  "2023",
+  { title: "Year", value: "" },
+  { title: "2015", value: "2015" },
+  { title: "2016", value: "2016" },
+  { title: "2017", value: "2017" },
+  { title: "2018", value: "2018" },
+  { title: "2019", value: "2019" },
+  { title: "2020", value: "2020" },
+  { title: "2021", value: "2021" },
+  { title: "2022", value: "2022" },
+  { title: "2023", value: "2023" },
+];
+
+export const fuels = [
+  {
+    title: "Fuel",
+    value: "",
+  },
+  {
+    title: "Gas",
+    value: "Gas",
+  },
+  {
+    title: "Electricity",
+    value: "Electricity",
+  },
 ];
 
 export const footerLinks = [
