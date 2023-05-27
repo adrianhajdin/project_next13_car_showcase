692815362ea76f5e1b33fc14dc13a63586a37353
TidbitsJS <tidbitjs@gmail.com>
2023-05-27 08:20:34 -0700

    navbar styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,8 +6,8 @@ import CustomButton from "./CustomButton";
 const NavBar = () => {
   return (
     <header className="w-full  absolute z-10">
-      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
-        <Link href="/" className="flex justify-center items-center">
+      <nav className="max-width flex-betweeb padding-x padding-y bg-transparent">
+        <Link href="/" className="flex-center">
           <Image
             src="/logo.svg"
             alt="logo"
