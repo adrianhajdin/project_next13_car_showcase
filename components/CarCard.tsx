1252d6b9d8627c206eeab1a553dc70b9fc15fbc7
TidbitsJS <tidbitjs@gmail.com>
2023-05-27 07:22:43 -0700

    car card styles

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -31,9 +31,9 @@ const CarCard = ({ car }: CarCardProps) => {
   const carRent = calculateCarRent(city_mpg, year);
 
   return (
-    <div className="group flex flex-col p-6 justify-center items-start text-black-100 bg-primary-blue-100 hover:bg-white hover:shadow-md rounded-3xl">
-      <div className="w-full flex justify-between items-start gap-2">
-        <h2 className="text-[22px] leading-[26px] font-bold capitalize">
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
           {make} {model}
         </h2>
 
@@ -47,17 +47,13 @@ const CarCard = ({ car }: CarCardProps) => {
         />
       </div>
 
-      <p className="flex mt-6 text-[32px] leading-[38px] font-extrabold">
-        <span className="self-start text-[14px] leading-[17px] font-semibold">
-          $
-        </span>
+      <p className="car-card__price">
+        <span className="car-card__price-dollar">$</span>
         {carRent}
-        <span className="self-end text-[14px] leading-[17px] font-medium">
-          /day
-        </span>
+        <span className="car-card__price-day">/day</span>
       </p>
 
-      <div className="relative w-full h-40 my-3 object-contain">
+      <div className="car-card__image">
         <Image
           src={`https://cdn.imagin.studio/getimage?customer=${imaginApiKey}&make=${make}&modelFamily=${
             model.split(" ")[0]
@@ -70,29 +66,29 @@ const CarCard = ({ car }: CarCardProps) => {
       </div>
 
       <div className="relative flex w-full mt-2">
-        <div className="flex group-hover:invisible w-full justify-between text-grey">
-          <div className="flex flex-col justify-center items-center gap-2">
+        <div className="car-card__icon-container">
+          <div className="car-card__icon">
             <Image
               src="/steering-wheel.svg"
               width={20}
               height={20}
               alt="steering wheel"
             />
-            <p className="text-[14px] leading-[17px]">
+            <p className="car-card__icon-text">
               {transmission === "a" ? "Automatic" : "Manual"}
             </p>
           </div>
-          <div className="flex flex-col justify-center items-center gap-2">
+          <div className="car-card__icon">
             <Image src="/tire.svg" width={20} height={20} alt="seat" />
-            <p className="text-[14px] leading-[17px]">{drive.toUpperCase()}</p>
+            <p className="car-card__icon-text">{drive.toUpperCase()}</p>
           </div>
-          <div className="flex flex-col justify-center items-center gap-2">
+          <div className="car-card__icon">
             <Image src="/gas.svg" width={20} height={20} alt="seat" />
-            <p className="text-[14px] leading-[17px]">{city_mpg} MPG</p>
+            <p className="car-card__icon-text">{city_mpg} MPG</p>
           </div>
         </div>
 
-        <div className="hidden group-hover:flex absolute bottom-0 w-full z-10">
+        <div className="car-card__btn-container">
           <CustomButton
             title="View More"
             containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
