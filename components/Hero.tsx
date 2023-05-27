2c1fa6bf8841f1204ff1dd3f34bd169e751eb1bc
TidbitsJS <tidbitjs@gmail.com>
2023-05-27 06:57:12 -0700

    hero styles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,13 +14,13 @@ const Hero = () => {
   };
 
   return (
-    <div className="flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
-      <div className="flex-1 pt-36 sm:px-16 px-6">
-        <h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold">
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
           Find, book, rent a car—quick and super easy!
         </h1>
 
-        <p className="text-[27px] text-black-100 font-light mt-5">
+        <p className="hero__subtitle">
           Streamline your car rental experience with our effortless booking
           process.
         </p>
@@ -31,12 +31,12 @@ const Hero = () => {
           handleClick={handleScroll}
         />
       </div>
-      <div className="xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen">
-        <div className="relative xl:w-full w-[90%] xl:h-full h-[590px] z-0">
+      <div className="hero__image-container">
+        <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
 
-        <div className="absolute xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden" />
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
