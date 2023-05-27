56a230432b44fe6f26d10087e2a0e1ebb6ee3657
TidbitsJS <tidbitjs@gmail.com>
2023-05-27 08:17:38 -0700

    footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,35 +4,32 @@ import Link from "next/link";
 import { footerLinks } from "@constants";
 
 const Footer = () => (
-  <footer className='flex flex-col text-black-100  mt-5 border-t border-gray-100'>
-    <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
-      <div className='flex flex-col justify-start items-start gap-6'>
+  <footer className="footer">
+    <div className="footer__links-container">
+      <div className="footer__rights">
         <Image
-          src='/logo.svg'
-          alt='logo'
+          src="/logo.svg"
+          alt="logo"
           width={118}
           height={18}
-          className='object-contain'
+          className="object-contain"
         />
-        <p className='text-base text-gray-700'>
+        <p className="text-base text-gray-700">
           Carhub 2023 <br />
           All Rights Reserved &copy;
         </p>
       </div>
 
-      <div className='flex-1 w-full flex md:justify-end flex-wrap max-md:mt-10 gap-20'>
+      <div className="footer__links">
         {footerLinks.map((item) => (
-          <div
-            key={item.title}
-            className='flex flex-col gap-6 text-20 min-w-[170px]'
-          >
-            <h3 className='font-bold'>{item.title}</h3>
-            <div className='flex flex-col gap-5'>
+          <div key={item.title} className="footer__link">
+            <h3 className="font-bold">{item.title}</h3>
+            <div className="flex flex-col gap-5">
               {item.links.map((link) => (
                 <Link
                   key={link.title}
                   href={link.url}
-                  className='text-gray-500'
+                  className="text-gray-500"
                 >
                   {link.title}
                 </Link>
@@ -43,14 +40,14 @@ const Footer = () => (
       </div>
     </div>
 
-    <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
+    <div className="footer__copyrights">
       <p>@2022 CarHub. All rights reserved</p>
 
-      <div className='flex-1 flex sm:justify-end justify-center max-sm:mt-4 gap-10'>
-        <Link href='/' className='text-gray-500'>
+      <div className="footer__copyrights-link">
+        <Link href="/" className="text-gray-500">
           Privacy & Policy
         </Link>
-        <Link href='/' className='text-gray-500'>
+        <Link href="/" className="text-gray-500">
           Terms & Condition
         </Link>
       </div>
