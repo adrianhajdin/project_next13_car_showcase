f32bd37cb407e36394d5f49d4f4cdbeb1cd99fc2
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-05-29 18:37:12 +0530

    model placeholder update

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -54,7 +54,7 @@ const SearchBar = ({ setManuFacturer, setModel }: SearchBarProps) => {
           name='model'
           value={searchModel}
           onChange={(e) => setSearchModel(e.target.value)}
-          placeholder='Taigun...'
+          placeholder='Tiguan...'
           className='searchbar__input'
         />
         <SearchButton otherClasses='sm:hidden' />
