bc21565e80e515db05bfa8c022c4df66c15e58e4
Ionuț <29517686+Ionut-B@users.noreply.github.com>
2023-07-05 14:46:49 +0300

    Update Footer.tsx - added "/" in "import { footerLinks } from "@/constants";"

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { footerLinks } from "@constants";
+import { footerLinks } from "@/constants";
 
 const Footer = () => (
   <footer className='flex flex-col text-black-100  mt-5 border-t border-gray-100'>
