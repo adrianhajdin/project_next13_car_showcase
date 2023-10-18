e0ff897103de1ac4a0dcf955c3b53e821adeda52
Anthony Yiu <59154982+anthonyiu@users.noreply.github.com>
2023-10-18 22:36:28 +0100

    Disable scroll to top when filtering
    add { scroll: false } to router.push() to improve UX

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -55,7 +55,7 @@ const SearchBar = () => {
     // Generate the new pathname with the updated search parameters
     const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
 
-    router.push(newPathname);
+    router.push(newPathname, { scroll: false });
   };
 
   return (
